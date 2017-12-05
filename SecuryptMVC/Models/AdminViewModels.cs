277e518648a4621f1e25083b50dc31d414374132
Kevin Mitchell <kfmitch@gmail.com>
2017-12-05 00:50:39 -0800

    added Admin button back to navbar

---
+++ SecuryptMVC/Models/AdminViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SecuryptMVC.Models
+{
+    public class AdminListViewModel
+    {
+        public List<AdminViewModel> AdminUserList { get; set; }
+    }
+
+    public class AdminViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public int AccessFailedCount { get; set; }
+    }
+}
